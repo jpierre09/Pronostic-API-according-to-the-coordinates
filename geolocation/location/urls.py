8ff9a1af2aba66de3e3bbec5d9f14526b24cd423
Jean Pierre <jeanlondono09@gmail.com>
2022-12-14 11:32:47 -0500

    Adición del archivo javascript de localicación al projecto en Django

--- geolocation/location/urls.py
+++ geolocation/location/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('geoposition/', views.geoposition, name='geoposition'),
 ]
--- geolocation/location/views.py
+++ geolocation/location/views.py
@@ -24,7 +24,7 @@ def index(request):
     res = requests.get('http://ip-api.com/json/')
     location_data_one   = res.text
     location_data       = json.loads(location_data_one)
-    print(location_data)
+    #print(location_data)
 
     # Api pronostico simplificado
     url_api_pronostico = 'http://siata.gov.co:8089/pronosticoMunicipiosSimplificado/63882184869634ff91bcf727d3fa210ec6c210bf/'
@@ -32,7 +32,13 @@ def index(request):
     api_pronostico = requests.get(url_api_pronostico)
     data_format_text = api_pronostico.text
     api_format_json = json.loads(data_format_text)
-    print(api_format_json)
-
+    #print(api_format_json)
+    print(type(api_format_json))
+    
+    ##### pruebas
 
     return render(request, 'index.html', {'data': location_data})
+
+
+def geoposition(request):
+    return render(request, 'geolocation.html')
